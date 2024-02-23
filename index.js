805a1c63cf72f66c27dcc67b58259a7b7ea789ba
parvejme24 <mdparvejme24@gmail.com>
2024-02-23 20:56:54 +0600

    connection to db successfull

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require("./src/app");
-const port = 6060;
+const connectDB = require("./src/config/db");
+require("dotenv").config();
+const port = process.env.PORT || 6060;
 
-app.listen(() => {
+app.listen(port, async () => {
   console.log(`gadget-hub server is running at http://localhost:${port}`);
+  await connectDB();
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,15 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const getConnectionString = () => {
-  let connectionURI;
-  if (process.env.NODE_ENV === "development") {
-    connectionURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ml6twf.mongodb.net/?retryWrites=true&w=majority`;
-  } else {
-    connectionURI = process.env.DATABASE_PROD_URI;
+const { mongodbURL } = require("../secret");
+const connectDB = async (options = {}) => {
+  try {
+    await mongoose.connect(mongodbURL, options);
+    console.log("connection to DB is successful...");
+    mongoose.connection.on("error", (error) => {
+      console.error("DB connection error", error);
+    });
+  } catch (error) {
+    console.error("coundl not connect to db", error.toString());
   }
-  return connectionURI;
 };
 
-const connectToDB = async () => {
-  console.log("connection to database...");
-  const uri = getConnectionString();
-  await mongoose.connect(uri, { dbName: process.env.DB_NAME });
-  console.log("connect to database...");
-};
-
-module.exports = connectToDB;
+module.exports = connectDB;
