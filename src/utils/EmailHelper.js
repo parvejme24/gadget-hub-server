08f15c1964db824fe4b1c6fcd0ce53441ab6a671
parvejme24 <mdparvejme24@gmail.com>
2024-02-24 16:15:18 +0600

    added token helper

---
+++ src/utils/EmailHelper.js
@@ -0,0 +1,20 @@
+const nodemailer = require("nodemailer");
+
+const EmailSend = async (EmailTo, EmailText, EmailSubject) => {
+  let transport = nodemailer.createTransport({
+    host: "mdparvejmep@gmail.com",
+    port: 25,
+    secure: false,
+    host: { user: "mdparvejmep@gmail.com", pass: "123456" },
+    tls: { rejectUnauthorized: false },
+  });
+
+  let mailOption = {
+    from: "GadgetHub <info@gadget-hub.com>",
+    to: EmailTo,
+    subject: EmailSubject,
+    text: EmailText,
+  };
+
+  return await transport.sendMail(mailOption);
+};
