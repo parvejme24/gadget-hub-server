e1e923bfe55ad0bbf8e1f0db524c9bdebdad0d48
parvejme24 <mdparvejme24@gmail.com>
2024-02-28 00:54:24 +0600

    added and install some  package

--- src/app.js
+++ src/app.js
@@ -3,7 +3,37 @@ const userRouter = require("./routers/UserRouter");
 const router = require("./routers/api");
 const app = new express();
 
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// middleware
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
+app.use(limiter);
+
+// all apis endpoints
 app.use("/api/v1", userRouter);
 app.use("/api/v1", router);
+
+app.get("*", (req, res) => {
+  res.status(401).json({ message: "Invalid URL" });
+});
 
 module.exports = app;
