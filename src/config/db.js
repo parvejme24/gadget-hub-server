bb515c9d31226b257236635e1c0e61a52f78355f
parvejme24 <mdparvejme24@gmail.com>
2024-03-17 19:02:38 +0600

    added db name

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const { mongodbURL } = require("../secret");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongodbURL);
+    await mongoose.connect(mongodbURL, { dbName: process.env.DB_NAME });
     console.log("Connection to DB is successful...");
 
     mongoose.connection.on("error", (error) => {
