b15003ec9df674bd4289f1331e127302eb53f389
parvejme24 <mdparvejme24@gmail.com>
2024-03-23 02:02:53 +0600

    updaet app.js file

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require("express");
-const app = new express();
-const router = require("./src/api");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-
-// security middlware import
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
 const cors = require("cors");
 const productRouter = require("./src/routers/ProductRouter");
+
+const app = express();
+
+// Middleware for security
 const corsOptions = {
   origin: ["*"],
   credentials: true,
   optionSuccessStatus: 200,
 };
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
 
-// security middleware use
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(helmet());
@@ -29,22 +28,24 @@ app.use(xss());
 app.use(hpp());
 app.use(limiter);
 
-// non security middleware use
+// Middleware for logging
 app.use(morgan("dev"));
+
+// Middleware for parsing request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
-// home route
-app.use("/", (req, res) => {
+// Home route
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Gadget-Hub Server" });
 });
 
-// api endpoints
+// API endpoints
 app.use("/api/v1", productRouter);
 
+// Middleware for handling invalid URLs
 app.get("*", (req, res) => {
-  res.status(401).json({ message: "Invalid URL" });
+  res.status(404).json({ message: "Invalid URL" });
 });
 
 module.exports = app;
