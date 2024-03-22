85ceeaa8151ffae380ba5eecdc4061b9fdf0323c
parvejme24 <mdparvejme24@gmail.com>
2024-03-23 03:03:04 +0600

    update index.js file

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const app = require("./app");
-const connectDB = require("./src/config/db");
-const { serverPort } = require("./src/secret");
+// const app = require("./app");
+// const connectDB = require("./src/config/db");
+// const { serverPort } = require("./src/secret");
 
-app.listen(serverPort, async () => {
-  console.log(`Gadget Hub is running at: http://localhost:${serverPort}`);
-  await connectDB();
-});
+// app.listen(serverPort, async () => {
+//   console.log(`Gadget Hub is running at: http://localhost:${serverPort}`);
+//   await connectDB();
+// });
+
+require("dotenv").config;
+const http = require("http");
+const app = require("./app");
+const connectToDB = require("./src/config/db");
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+
+const main = async () => {
+  await connectToDB();
+  server.listen(port, () => {
+    console.log(`Gadget Hub server is running on: http://localhost:${port}`);
+  });
+};
+
+main();
