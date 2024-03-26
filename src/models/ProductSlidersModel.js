392574ac966553dae4860e5c58f3930f48813d91
parvejme24 <mdparvejme24@gmail.com>
2024-03-26 16:07:44 +0600

    added product slider model

--- src/models/ProductSlidersModel.js
+++ src/models/ProductSlidersModel.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 
-const ProductSlidersSchema = mongoose.Schema({});
+const ProductSlidersSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: String,
+    },
+  },
+  { timestamps: true, versionKey: true }
+);
 
 const ProductsSlidersModel = mongoose.model(
   "productSliders",
