99f59857a0618c266435e74d45ef5a0b24211b6e
Md Parvej <mdparvejme24@gmail.com>
2024-05-04 23:00:47 +0600

    added query product by category and brand

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -75,3 +75,25 @@ exports.deleteProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// find/query products accoridint to the category
+exports.getProductsByCategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// find/query products accoridint to the brand
+exports.getProductsByBrand = async (req, res) => {
+  try {
+    const { brand } = req.params;
+    const products = await Product.find({ brand });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
