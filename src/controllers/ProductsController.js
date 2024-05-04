292b9621a14bc8b5b331a52a7cc547e928dd53b8
Md Parvej <mdparvejme24@gmail.com>
2024-05-04 22:49:20 +0600

    added get product by id and title controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -19,3 +19,30 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// get a product by id
+exports.getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get a product by title
+exports.getProductByTitle = async (req, res) => {
+  try {
+    const { title } = req.params;
+    const product = await Product.findOne({ title });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json({ product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
