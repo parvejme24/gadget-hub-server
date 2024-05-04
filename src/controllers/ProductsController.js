62058a70a93516b8b4b37f8acd5ba8ecc775c8f1
Md Parvej <mdparvejme24@gmail.com>
2024-05-04 22:56:46 +0600

    added update and delete product controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -46,3 +46,32 @@ exports.getProductByTitle = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// update product details by id
+exports.updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// delete product by id
+exports.deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
