5cfec3a4b9e23b89d9ad3b30d5f5d56c99a957f0
Md Parvej <mdparvejme24@gmail.com>
2024-05-04 22:40:44 +0600

    added add new product controller

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,41 +1,11 @@
-const BrandModel = require("../models/BrandModel");
-const {
-  BrandListService,
-  CategoryListService,
-  SliderListServicee,
-  ProductListService,
-  ListByBrandService,
-  ListByCategoryService,
-} = require("../services/ProductServices");
+const Product = require("../models/ProductsModel");
 
-exports.ProductBrandList = async (req, res) => {
-  let result = await BrandListService();
-  return res.status(200).json(result);
+// add new product
+exports.addProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
-
-exports.ProductCategoryList = async (req, res) => {
-  let result = await CategoryListService();
-  return res.status(200).json(result);
-};
-
-exports.ProductSliderList = async (req, res) => {
-  let result = await SliderListServicee();
-  return res.status(200).json(result);
-};
-
-exports.ProductListByBrand = async (req, res) => {
-  let result = await ListByBrandService();
-  return res.status(200).json(result);
-};
-
-exports.ProductListByCategory = async (req, res) => {
-  let result = await ListByCategoryService();
-  return res.status(200).json(result);
-};
-
-exports.ProductListBySemilier = async (req, res) => {};
-exports.ProductListByKeywork = async (req, res) => {};
-exports.ProductReviewList = async (req, res) => {};
-exports.ProductListByRemark = async (req, res) => {};
-exports.ProductDetails = async (req, res) => {};
-exports.CreateProductReview = async (req, res) => {};
